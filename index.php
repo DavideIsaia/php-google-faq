7c84dd1766f5bbfb3e184a948e228a11aad46b0a
DavideIsaia <davide.enjoy@hotmail.it>
2022-06-09 15:11:35 +0200

    stilizzato header

--- index.php
+++ index.php
@@ -9,9 +9,33 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Google FAQ</title>
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  
+  <!-- header -->
+  <header>
+    <div class="clearfix h-top">
+      <!-- logo -->
+      <div class="col col-left logo">
+        <img src="img/logo.jpg" alt="logo">
+      </div>
+      <!-- utente -->
+      <div class="col col-right user">
+        <img src="img/user.jpg" alt="user">
+      </div>
+    </div>
+    <nav class="clearfix">
+      <!-- menu -->
+      <div class="col">
+        <ul class="header-menu">
+          <li>Introduzione</li>
+          <li>Norme sulla Privacy</li>
+          <li>Termini di servizio</li>
+          <li>Tecnologie</li>
+          <li>Domande Frequenti</li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </body>
 </html>
